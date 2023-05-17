20d3ef37f6fc90c9e70d053df1feea12710e2a00
bluegalaxy13 <ricardosassofficial@gmail.com>
2023-05-17 12:43:05 +0200

    Update 1-search_replace.py

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -4,8 +4,8 @@ def search_replace(my_list, search, replace):
     """
     replaces all occurances of search with replace
     """
-    new_list = my_list
-    for idx, item in enumerate(new_list):
+    n_list = my_list
+    for idx, item in enumerate(n_list):
         if item == search:
-            new_list[idx] = replace
-    return (new_list)
+            n_list[idx] = replace
+    return (n_list)
