ceeceb047bce4ca98f2739082cc0d34170809c3e
bluegalaxy13 <ricardosassofficial@gmail.com>
2023-05-17 12:33:09 +0200

    Update 1-search_replace.py

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -4,4 +4,5 @@ def search_replace(my_list, search, replace):
     """
     replaces all occurances of search with replace
     """
-    return ([elem if elem != search else replace for elem in my_list])
+    new_list = my_list.replace(search, replace)
+    return (new_list)
