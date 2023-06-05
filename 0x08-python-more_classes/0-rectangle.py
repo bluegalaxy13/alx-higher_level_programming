002e4cf1e2eee0691627a5d782584a1dfa1e9ece
bluegalaxy13 <ricardosassofficial@gmail.com>
2023-06-05 18:41:44 +0200

    Finished Project 0x08

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""
+@author: Ricardo Sass
+"""
+
+
+class Rectangle:
+    """Empty class Rectangle that defines a rectangle figure
+
+    Attributes:
+        empty
+    """
+
+    pass
