070ff12d628bec044d3117fca33be3e54e61fc0e
bluegalaxy13 <ricardosassofficial@gmail.com>
2023-06-05 15:16:59 +0200

    Finished Python Project 0x07

---
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+"""
+@author: Ricardo Sass
+"""
+from numpy import matmul
+
+
+def lazy_matrix_mul(m_a, m_b):
+    """
+    Multiply two matrix
+
+    Args:
+        m_a (list): First matrix
+        m_b (list): second matrix
+
+    Returns:
+        The result of multiply m_a by m_b
+
+    """
+    return matmul(m_a, m_b)
