b1309c8997fe6a5d1d02dc65e3ff24a6eb74ee3b
bluegalaxy13 <ricardosassofficial@gmail.com>
2023-11-08 14:23:13 +0200

    changed part 1 of 0x04

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -2,10 +2,13 @@
 
 def search_replace(my_list, search, replace):
     """
-    replaces all occurances of search with replace
+    A function that replaces all occurrences
+    of an element by another in a new list
     """
-    n_list = my_list
-    for idx, item in enumerate(n_list):
-        if item == search:
-            n_list[idx] = replace
-    return (n_list)
+    new_list = []
+    for element in my_list:
+        if element == search:
+            new_list.append(replace)
+        else:
+            new_list.append(element)
+    return new_list
