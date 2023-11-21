e416a9bf7ab5e268aa49e871f704312121ab35c5
bluegalaxy13 <ricardosassofficial@gmail.com>
2023-11-21 17:16:16 +0200

    changed spacing

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -3,13 +3,10 @@
 Created on Tue November 21 16:00:00 2023
 @author: Ricardo Sass
 """
-
-
 class Square:
     """Empty class Square that defines a square
 
         Attributes:
         empty
-    """
-    
+    """  
     pass
