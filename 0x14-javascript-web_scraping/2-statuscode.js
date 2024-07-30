dc108c8cea9e153dbf2091aa711e7f3be23d226a
ricardo <ricardosassofficial@gmail.com>
2024-07-30 05:01:48 -0700

    Finished task 3

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -3,15 +3,10 @@
 const request = require('request');
 const url = process.argv[2];
 
-request(url, function (err, response, body) 
-{
-  if (err) 
-  {
-    console.log(err);
-  } 
-  
-  else 
-  {
-    console.log('code: ' + response.statusCode);
+request(url, (err, response) => {
+  if (err) {
+    console.error(err); // Use console.error for error logging
   }
+
+  console.log('code: ' + response.statusCode);
 });
