b6d23684d1e2d356f5471585883276000319dec4
ricardo <ricardosassofficial@gmail.com>
2024-07-30 04:35:01 -0700

    Added task 2

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -2,10 +2,9 @@
 
 const fs = require('fs');
 
-fs.writeFile(process.argv[2], process.argv[3], err => 
-{
-  if (err) 
-  {
+fs.writeFile(process.argv[2], process.argv[3], err => {
+  if (err) {
     console.log(err);
+    return; // Added return to exit early if there's an error
   }
 });
